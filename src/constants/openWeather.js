37fef2b5742f56024986f96c57a41e6672c9f0f2
Jaime García Estévez <jge6996@hotmail.com>
2020-12-29 12:40:33 +0100

    work in progress

--- src/constants/openWeather.js
+++ src/constants/openWeather.js
@@ -1,20 +1,8 @@
-BASE_URL = 'http://api.openweathermap.org/data/2.5/' 
-BASE_URL_2 = 'https://api.openweathermap.org/data/2.5/'
+
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/';
 
 const API_ID = process.env.API_ID;
 
-// Para poder obtener los datos que queremos necesitamos dos direcciones distintas de la API de OpenWeather,
-// la primera que vamos a utilizar será para obtener las coordenadas, a continuación  las utilizaremos para
-// obtener los datos que queremos actuales y de 7 días en adelante.
+const getWeather = (city) => `${BASE_URL}forecast?q=${city},ES&appid=${API_ID}`;
 
-const getCitiesCoords = (city) => `${BASE_URL}weather?q=${city},ES&appid=${API_ID}`;
-
-const getCitiesWeather = (lat, lon) =>
-  `${BASE_URL_2}onecall?lat=${lat}&lon=${lon}&exclude='minutely','hourly','alert'&appid=${API_ID}`;
-
-// Ahora exportamos ambas constantes
-
-module.exports = {
-    getCitiesCoords,
-    getCitiesWeather
-};
+module.exports = {getWeather} ;
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,53 +1,46 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
+const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
 const cities = require('../constants/cities');
-const { getCitiesCoords, getCitiesWeather } = require('../constants/openWeather');
+const { getWeather } = require('../constants/openWeather');
+const { json } = require('express');
 
 // Hacemos este script el cual sería interesante llamar una vez al día para traer información lo más actual posible
 
 const dailyScript = async () => {
   try {
-    const citiesCoordsPromises = cities.map((city) => {
-      const coordApi = getCitiesCoords(city);
-      return fetch(coordApi)
-        .then((res) => res.json())
-        .then((content) => console.log(content));
+    const citiesWeather = await Promise.all(
+      cities.map(async (city) => {
+        const citiesApi = getWeather(city);
+        const cityResult = await axios.get(citiesApi, {
+          headers: {
+            Accept: '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            Connection: 'keep-alive',
+          },
+        });
+
+        return cityResult.data;
+      })
+    );
+    const mapCitiesWeather = citiesWeather.map((city) => {
+      const i = city['list'].lenght;
+      console.log(i);
+      return {
+        
+        name: city.city.name,
+        date: city['list'][i]['dt'],
+        threeHoursDate: city['list'][i]['dt_txt'],
+        temperature: city['list'][i]['temp']
+      };
     });
 
-    const citiesCoords = await Promise.all(citiesCoordsPromises);
-    const citiesCoordsResult = citiesCoords.map(({ name, coord }) => ({
-      name,
-      lon: coord.lon,
-      lat: coord.lat,
-    }));
-    console.log(citiesCoordsResult);
+    console.log(citiesWeather);
 
-    const weatherPromise = citiesCoordsResult.map(({ lat, lon }) => {
-      const weatherApi = getCitiesWeather(lat, lon);
-      return fetch(weatherApi).then((res) => res.json());
-    });
-
-    const citiesWeather = await Promise.all(weatherPromise);
-
-    // Hemos utilizado el mismo proceso dos veces con promesas, ya que primero se realiza una entera y luego la siguiente,
-    // y en caso de haya algún problema se parará y saltará un error donde hay fallado. Este erro lo hará el catch, más abajo *
-
-    // El siguiente paso tendrá que ser formatear los datos que nos trae la API ya que no llegan como esperamos.
-
-    const result = citiesCoordsResult.map((coordResult, index) => ({
-      ...coordResult,
-      daily: citiesWeather[index].daily.map(({ dt, temp }) => ({
-        date: new Date(dt * 1000), // Multiplicamos por 1000 porque viene en segundo de la API
-        temp: temp.day, // Viene en Farenheits (hay más para cada momento del día: min, max, night...)
-      })),
-    }));
-    console.log(result);
-
-    const filePath = path.join(__dirname);
-    fs.writeFileSync(filePath, JSON.stringify(result));
+    const filePath = path.join(__dirname, './weather.json');
+    fs.writeFileSync(filePath, JSON.stringify(mapCitiesWeather));
   } catch (err) {
     console.log('Weather dailyScript error:', err);
   }
