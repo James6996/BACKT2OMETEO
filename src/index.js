45194803f0ac986f6b7234aa63001d84ea2ca299
Jaime García Estévez <jge6996@hotmail.com>
2020-12-30 18:11:36 +0100

    dumping data in the database and ending the backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const MongoStore = require('connect-mongo')
-const mongoose = require('mongoose')
-
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://t2ometeo-forecast:t2ometeo-forecast@t2ometeo.zrpjh.mongodb.net';
 
 const app = express();
 
@@ -12,8 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // Introduciendo cors podremos usar nuestro server desde el front
 
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  console.log('Connected to BD!!');
+  db.close();
+});
 
-const PORT = Number(process.env.PORT || 3000); 
+const PORT = Number(process.env.PORT || 3000);
 // Declaro el puerto como tipo number, añadimos una variable de entorno o en su defecto el puerto 3000
 app.listen(PORT, () => {
   console.log(`Server listening in http://localhost:${PORT}`);
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://t2ometeo-forecast:t2ometeo-forecast@t2ometeo.zrpjh.mongodb.net';
 const cities = require('../constants/cities');
 const { getWeather } = require('../constants/openWeather');
 
@@ -25,6 +26,25 @@ const dailyScript = async () => {
       })
     );
     const mapCitiesWeather = citiesWeather.map((city) => {
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("t2ometeo-forecast");
+        var myobj = {
+          name: city.city.name,
+          forecast: city.list.map((forecast) => {
+            return {
+              date: forecast.dt,
+              temperature: (forecast.main.temp - 273.15).toFixed(2),
+              dateThreeHours: forecast.dt_txt,
+            };
+          })
+        }
+        dbo.collection("forecast").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
       return {
         name: city.city.name,
         forecast: city.list.map((forecast) => {
